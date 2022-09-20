cb35bb9b87d709bba5c89aa333a7ee4bade01ea0
kadirzkaya <bmkozkaya@gmail.com>
2022-09-20 12:55:52 +0300

    Initial commit

---
+++ src/EnYakinSayilariBulma.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EnYakinEnUzakSayiyiBulma {
+    public static void main(String[] args) {
+        int[] sayiDizisi={15,12,788,1,-1,-778,2,0};
+        int girilenSayi,enYakin,enUzak;
+        Arrays.sort(sayiDizisi);
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Girilen sayı : ");
+        girilenSayi=sc.nextInt();
+        for (int i:sayiDizisi){
+            if (girilenSayi<sayiDizisi[i]){
+                en
+            }
+        }
+    }
+}
